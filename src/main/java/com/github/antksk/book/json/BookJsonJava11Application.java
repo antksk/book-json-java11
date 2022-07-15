bd529c24280c3237b6483dd29abde99167f27c4b
newt <antksk@gmail.com>
2022-07-15 14:39:05 +0900

    init

---
+++ src/main/java/com/github/antksk/book/json/BookJsonJava11Application.java
@@ -0,0 +1,13 @@
+package com.github.antksk.book.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookJsonJava11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookJsonJava11Application.class, args);
+    }
+
+}
